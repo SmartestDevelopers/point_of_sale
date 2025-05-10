a7b833206c48bcd6b7b79359c59c6d760ab16697
nosheeniftikhar <nosheeniftikhar00@gmail.com>
2025-05-10 04:53:16 -0700

    created controllers only
    created controllers.

--- pos/routes/web.php
+++ pos/routes/web.php
@@ -21,3 +21,36 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/', 'FrontController@index')->name('index');
 
+Route::get('/catalog', 'CatalogController@index')->name('index');
+
+Route::get('/expense', 'ExpenseController@index')->name('index');
+
+Route::get('/media', 'MediaController@index')->name('index');
+
+Route::get('/people', 'PeopleController@index')->name('index');
+
+Route::get('/report', 'ReportController@index')->name('index');
+
+Route::get('/record', 'RecordController@index')->name('index');
+
+Route::get('/quotation', 'QuotationController@index')->name('index');
+
+Route::get('/purchase', 'PurchaseController@index')->name('index');
+
+Route::get('/return', 'ReturnController@index')->name('index');
+
+Route::get('/sell-order', 'SellOrderController@index')->name('index');
+
+Route::get('/setting', 'SettingController@index')->name('index');
+
+
+
+
+
+
+
+
+
+
+
+
