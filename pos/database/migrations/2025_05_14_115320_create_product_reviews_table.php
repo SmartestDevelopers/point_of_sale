9abd8a4e91173c66f0cfd9ecada6887ab162fb5a
nosheeniftikhar <nosheeniftikhar00@gmail.com>
2025-05-14 04:56:09 -0700

    created product categories  and reviews table

---
+++ pos/database/migrations/2025_05_14_115320_create_product_reviews_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_reviews', function (Blueprint $table) {
+            $table->id();
+            $table->string('orders_id');
+            $table->string('product_names');
+            $table->string('reviews_text');
+            $table->string('date');
+            $table->string('action');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_reviews');
+    }
+}
