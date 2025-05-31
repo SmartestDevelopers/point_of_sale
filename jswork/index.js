801cab2f80d08430e4416574495a4acd1e992989
nosheeniftikhar <nosheeniftikhar00@gmail.com>
2025-05-31 05:02:46 -0700

    jswork

---
+++ jswork/index.js
@@ -0,0 +1,17 @@
+
+//document.write("Welcome to JS");
+//alert("This is an alert");
+//prompt("What's your date of birth?");
+//confirm("Are you sure you want to leave this page?");
+
+//let word = alert("Beware this might harm your PC");
+//console.log(word);
+
+//let reason = prompt("What's your father's name?");
+//console.log(reason);
+
+//let truth = confirm("Are your sure?");
+//console.log(truth);
+
+//Different Cases
+// pascal case
